3636554a79c8f0acd8c63c2c0a26b49c4392addb
Esmeralda-Almaguer <esmeraldaa.tecno30@gmail.com>
2022-12-02 12:55:13 -0600

    Catalogo completo

--- src/java/Controller/Controlador.java
+++ src/java/Controller/Controlador.java
@@ -5,8 +5,12 @@
  */
 package Controller;
 
+import Model.Producto;
+import Model.ProductoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,21 +31,24 @@ public class Controlador extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    ProductoDAO pdao = new ProductoDAO();
+    List<Producto> productos = new ArrayList<>();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+     
+            String accion = request.getParameter("accion");
+            productos = pdao.listar();
+            switch (accion){
+                case "ejemplo":
+                    break;
+                default:
+                    request.setAttribute("productos",productos);
+                    request.getRequestDispatcher("index.jsp").forward(request, response);
+            }
+            
+
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
